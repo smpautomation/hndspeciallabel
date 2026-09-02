3274940e2575d69621cb2ffeef99fb5c55d64f97
Edzel <animatidus@gmail.com>
2026-09-02 08:24:27 +0800

    revert '-' in $lot number

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -126,8 +126,8 @@ class PrintController extends Controller
         $Model = $data['model_name'];
         $Fixed_Value = $data['fixed_value'];
         $Quantity = $data['quantity'];
-        //$LotNo = $data['lot'];
-        $LotNo = str_replace("-", "", $data['lot']);
+        $LotNo = $data['lot'];
+        //$LotNo = str_replace("-", "", $data['lot']);
 
         $xData1 = str_pad(substr($Fixed_Value, 0, 3), 20, " ", STR_PAD_RIGHT);
         $xData2 = str_pad(substr($Fixed_Value, 3, 1), 22, " ", STR_PAD_RIGHT);
